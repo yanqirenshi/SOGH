a08cddcb28a41d747c2a35d60a57186aa2650e1d
yanqirenshi <yanqirenshi@gmail.com>
2021-03-15 10:47:34 +0900

    ver 0.0.15

--- src/components/ProductBacklogs.js
+++ src/components/ProductBacklogs.js
@@ -7,11 +7,13 @@ import Contents from './product_backlogs/Contents.js';
 export default function ProductBacklogs (props) {
     const sogh = props.sogh;
 
+    const url_prefix = props.productbacklog_url_prefix || "/product-backlogs/";
+
     return (
         <>
           {sogh  && <Contents sogh={props.sogh}
                               repository={props.repository}
-                              productbacklog_url_prefix="/product-backlogs/" />}
+                              productbacklog_url_prefix={url_prefix} />}
           {!sogh && <NotSignIn />}
         </>
     );
