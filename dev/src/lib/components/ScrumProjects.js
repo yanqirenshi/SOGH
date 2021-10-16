dc35ce4ce8eafc9f682f5317ce5d0a88e7c21f96
yanqirenshi <yanqirenshi@gmail.com>
2021-10-17 08:27:54 +0900

    rm Contents.js

--- dev/src/lib/components/ScrumProjects.js
+++ dev/src/lib/components/ScrumProjects.js
@@ -1,8 +1,30 @@
 import React, { useState, useEffect } from 'react';
 
-import NotSignIn from './common/NotSignIn.js';
+import NotSignIn           from './common/NotSignIn.js';
+import ButtonRefresh       from './common/ButtonRefresh.js';
+import ProductBacklogs     from './common/ProductBacklogs.js';
+import ProductAndMilestone from './common/ProductAndMilestone.js';
+import Filter              from './common/Filter.js';
+import ChartBardown        from './common/ChartBardown.js';
+import OperatorOpenClose   from './common/OperatorOpenClose.js';
+import Summary             from './common/Summary.js';
 
-import Contents from './scrum_projects/Contents.js';
+const style = {
+    root: {
+        marginBottom:111
+    },
+    head: {
+        padding: '11px 22px 0px 22px'
+    },
+    controller: {
+        padding: '0px 22px 8px 22px',
+        display:'flex',
+    },
+    body: {
+        display:'flex',
+        padding: '11px 22px'
+    },
+};
 
 export default function ScrumProjects (props) {
     const [scrum, setScrum] = useState(null);
@@ -16,10 +38,9 @@ export default function ScrumProjects (props) {
     const refresh = ()=> scrum.fetch(repository, ()=> invokeUpdate());
 
     useEffect(()=> sogh && setScrum(sogh.scrum()), [repository]);
+    useEffect(()=> { if (scrum) refresh(); }, [scrum]);
 
-    useEffect(()=> {
-        if (scrum) refresh();
-    }, [scrum]);
+    if (!scrum) return <NotSignIn/>;
 
     const callbacks = {
         refresh: ()=> refresh(),
@@ -46,14 +67,66 @@ export default function ScrumProjects (props) {
 
     const url_prefix = productbacklog_url_prefix || "/product-backlogs/";
 
+    const base = scrum._data;
+    const issues = base.issues;
+    const milestone = base.milestone;
+    const milestones = base.milestones;
+
+    const data = scrum._projects;
+
+    const projects_filterd = data.projects_filterd;
+    const filter = data.filter;
+
+    const sorted_projects_filterd = sogh.sortProjectsByPriority(projects_filterd.list);
+
     return (
-        <>
-          <span style={{display:'none'}}>{!!updated_at}</span>
-          {scrum  && <Contents scrum={scrum}
-                               callbacks={callbacks}
-                               repository={repository}
-                               productbacklog_url_prefix={url_prefix} />}
-          {!scrum && <NotSignIn />}
-        </>
+        <div style={style.root}>
+          <div style={style.head}>
+            <ProductAndMilestone repository={repository}
+                                 milestones={milestones}
+                                 milestone={milestone}
+                                 callbacks={callbacks} />
+          </div>
+
+          <div style={style.controller}>
+            <div>
+              <ButtonRefresh callbacks={callbacks} />
+            </div>
+
+            <Filter issues={issues}
+                    filter={filter}
+                    callbacks={callbacks}
+                    sogh={sogh} />
+          </div>
+
+          <div style={style.body}>
+
+            <div style={{flexGrow:1}}>
+              <div>
+                <ChartBardown issues={issues}
+                              milestone={milestone} />
+              </div>
+
+              <div>
+                <OperatorOpenClose callbacks={callbacks.projects} />
+
+                <div style={{display:'flex'}}>
+                  <div>
+                    <Summary type="projects"
+                             source={scrum.summaryProjects(sorted_projects_filterd)}/>
+                  </div>
+
+                  <div style={{flexGrow:1}}>
+                    <ProductBacklogs projects={sorted_projects_filterd}
+                                     close_projects={data.close_projects}
+                                     callbacks={callbacks}
+                                     sogh={sogh}
+                                     productbacklog_url_prefix={props.productbacklog_url_prefix} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
     );
 }
